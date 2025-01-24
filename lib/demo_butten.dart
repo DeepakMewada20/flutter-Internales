51d9127f5aa21c947cdac202244e3a7d212e72be
deepakmewada <mewadadeepak367@gmail.com>
2025-01-24 20:45:06 +0530

    I learn when build() method call again and again then is chech every widget for any change occrer or not and unneccesary time wiest, then only which widget change only this widget make statefulwidget and also remaning widget make stateleswidget

---
+++ lib/demo_butten.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+class DemoButten extends StatefulWidget {
+  const DemoButten({super.key});
+
+  @override
+  State<DemoButten> createState() {
+    return _DemoButten();
+  }
+}
+
+class _DemoButten extends State<DemoButten> {
+  bool _ishaveNolage = false;
+  void _isture(bool value) {
+    setState(() {
+      _ishaveNolage = value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: () {
+                _isture(false);
+              },
+              child: const Text("No"),
+            ),
+            const SizedBox(
+              width: 25,
+            ),
+            TextButton(
+              onPressed: () {
+                _isture(true);
+              },
+              child: const Text("Yes"),
+            ),
+          ],
+        ),
+        if (_ishaveNolage) const Text("Awesome!"),
+      ],
+    );
+  }
+}
--- lib/ui_updates_demo.dart
+++ lib/ui_updates_demo.dart
@@ -1,23 +1,44 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_internals/demo_butten.dart';
 
-class UiUpdatesDemo extends StatefulWidget {
+class UiUpdatesDemo extends StatelessWidget {
   const UiUpdatesDemo({super.key});
-  @override
-  State<StatefulWidget> createState() {
-    return _UiUpdatesDemo();
-  }
-}
+  
 
-class _UiUpdatesDemo extends State<UiUpdatesDemo> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Column(
-        //crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text("my Data"),
-        ],
+    return const Padding(
+      padding:  EdgeInsets.all(15.0),
+      child: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+             Text(
+              "Every Flutter devloper should have a basic understenting of flutter internals!",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 15,
+              ),
+            ),
+             SizedBox(
+              height: 20,
+            ),
+             Text(
+              "Do you Understed how flutter Update UI! ",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+             SizedBox(
+              height: 20,
+            ),
+            DemoButten(),
+          ],
+        ),
       ),
     );
   }
